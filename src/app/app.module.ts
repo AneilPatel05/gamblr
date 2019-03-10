9f9a12a3a5f7a292f0e137ded5686c025e887eb1
Aneilpatel05 <anil.patel.gty@gmail.com>
2019-03-10 10:19:24 -0400

    added type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -15,7 +15,13 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     StatusBar,
--- src/app/dicepage/dicepage.page.ts
+++ src/app/dicepage/dicepage.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dicepage',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DicepagePage implements OnInit {
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) { }
+  
+  diceForm: FormGroup;
+  amount:number=0.100;
+  coin:string='';
+  type:string='';
+  roll:number=null;
+  payout:number=null;
+  winChance:number=null;
   ngOnInit() {
+    this.diceForm = this.formBuilder.group({
+      'amount' : [null, Validators.required],
+      'coin' : [null, Validators.required],
+      'type' : [null, Validators.required],
+      'roll' : [null, Validators.required]
+    });
+    this.diceForm.get('amount').setValue(this.amount);
+    this.diceForm.get('coin').setValue(this.coin);
+    this.diceForm.get('type').setValue(this.type);
+    this.diceForm.get('roll').setValue(this.roll);
+    
   }
 
 }
